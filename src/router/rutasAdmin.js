2a52e84a3cecc9c5fab9298c87751522633dbfa9
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-01-15 17:19:46 -0500

    Eliminando líneas innecesarias

--- src/router/rutasAdmin.js
+++ src/router/rutasAdmin.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-// const Statistic = require('../models/statistic');
-// const Sale = require('../models/sale');
 const Product = require('../models/Product');
 const Users = require('../models/User');
 const multer = require('multer');
@@ -89,59 +87,59 @@ router.get('/usuariosRegistrados/eliminarUsuario', async (req, res) => {
 
 
 
-router.get('/ganancias', isAuthenticated, async (req, res) => {
+// router.get('/ganancias', isAuthenticated, async (req, res) => {
 
-    try {
-        //------------- Estadisticas ----------------
-    //Query para recibir las estadisticas de la Coleccion 'Statistics'
-    const stats = await Statistic.find({});
+//     try {
+//         //------------- Estadisticas ----------------
+//     //Query para recibir las estadisticas de la Coleccion 'Statistics'
+//     const stats = await Statistic.find({});
 
-    //------------- Ventas ----------------
-    const año = (new Date()).getFullYear();//Año actual
-    const sales = await Sale.find({'year':año});
+//     //------------- Ventas ----------------
+//     const año = (new Date()).getFullYear();//Año actual
+//     const sales = await Sale.find({'year':año});
 
-    //Cacular las VENTAS por MES del año actual
-    const sales_per_month = monthSales(sales);
+//     //Cacular las VENTAS por MES del año actual
+//     const sales_per_month = monthSales(sales);
     
-    //------------ VENTAS POR PRODUCTOS------------
-    /*
-        Hubiera querido poner esto en una funcion aparte,
-        pero no logre hacerlo, al final tuve que colocarlo todo
-        aqui mismo
-     */
-    //Recibo la lista de productos que existen en la tienda
-    const productNames = await Product.find({}).select('name -_id');
-    //Creo un arreglo donde guardare el conteo de cuantas veces
-    //se ha comprado un determinado curso.
-    var sales_by_product = []
+//     //------------ VENTAS POR PRODUCTOS------------
+//     /*
+//         Hubiera querido poner esto en una funcion aparte,
+//         pero no logre hacerlo, al final tuve que colocarlo todo
+//         aqui mismo
+//      */
+//     //Recibo la lista de productos que existen en la tienda
+//     const productNames = await Product.find({}).select('name -_id');
+//     //Creo un arreglo donde guardare el conteo de cuantas veces
+//     //se ha comprado un determinado curso.
+//     var sales_by_product = []
 
-    //Recorro la lista de nombres de productos en la tienda
-    for (const element of productNames) {
-        //Busco en la coleccion de venta, los productos con el nombre dado.
-        const query = await Sale.find({'products': element.name});
-        //Determino el tamaño de el objeto recibido de la BD
-        count = Object.keys(query).length
-        //Hago un objeto en donde guardo el nombre del curso y las veces que se ha comprado.
-        var product = {
-            name: element.name,
-            count: count
-        }
-        //Lo agrego al arreglo que se ha declarado anteriormente.
-        sales_by_product.push(product);
+//     //Recorro la lista de nombres de productos en la tienda
+//     for (const element of productNames) {
+//         //Busco en la coleccion de venta, los productos con el nombre dado.
+//         const query = await Sale.find({'products': element.name});
+//         //Determino el tamaño de el objeto recibido de la BD
+//         count = Object.keys(query).length
+//         //Hago un objeto en donde guardo el nombre del curso y las veces que se ha comprado.
+//         var product = {
+//             name: element.name,
+//             count: count
+//         }
+//         //Lo agrego al arreglo que se ha declarado anteriormente.
+//         sales_by_product.push(product);
         
-    }
-    //Pequeña validacion para que un CLIENTE no ingrese a esta ruta.
-    if(req.user.role != 'client'){ 
-        res.render('admin/ganancias',{stats,sales_per_month,sales_by_product});
-    }
-    else{
-        res.redirect('/');
-    }
+//     }
+//     //Pequeña validacion para que un CLIENTE no ingrese a esta ruta.
+//     if(req.user.role != 'client'){ 
+//         res.render('admin/ganancias',{stats,sales_per_month,sales_by_product});
+//     }
+//     else{
+//         res.redirect('/');
+//     }
 
-    } catch (error) {
-        console.log(error);
-    }
-});
+//     } catch (error) {
+//         console.log(error);
+//     }
+// });
 
 router.get('/administrarCursos', isAuthenticated, async (req, res) => {
     
@@ -166,57 +164,57 @@ router.get('/administrarCursos', isAuthenticated, async (req, res) => {
 });
 
 //Funcion para calcular las VENTAS por MES del año actual
-function monthSales(sales){
+// function monthSales(sales){
 
-    //Inicializacion de arreglo con 12 indices (12 meses)
-    sales_per_months = [0,0,0,0,0,0,0,0,0,0,0,0]
+//     //Inicializacion de arreglo con 12 indices (12 meses)
+//     sales_per_months = [0,0,0,0,0,0,0,0,0,0,0,0]
 
-    sales.forEach(sale => {
-        //Suma + 1 debido a que los meses se almacenan del 0 al 11
-        month = sale.month + 1;
+//     sales.forEach(sale => {
+//         //Suma + 1 debido a que los meses se almacenan del 0 al 11
+//         month = sale.month + 1;
 
-        switch(month){
-            case 1:
-                sales_per_months[0] += sale.total_purchased
-            break;
-            case 2:
-                sales_per_months[1] += sale.total_purchased
-            break;
-            case 3:
-                sales_per_months[2] += sale.total_purchased
-            break;
-            case 4:
-                sales_per_months[3] += sale.total_purchased
-            break;
-            case 5:
-                sales_per_months[4] += sale.total_purchased
-            break;
-            case 6:
-                sales_per_months[5] += sale.total_purchased
-            break;
-            case 7:
-                sales_per_months[6] += sale.total_purchased
-            break;
-            case 8:
-                sales_per_months[7] += sale.total_purchased
-            break;
-            case 9:
-                sales_per_months[8] += sale.total_purchased
-            break;
-            case 10:
-                sales_per_months[9] += sale.total_purchased
-            break;
-            case 11:
-                sales_per_months[10] += sale.total_purchased
-            break;
-            case 12:
-                sales_per_months[11] += sale.total_purchased
-            break;
-        }
+//         switch(month){
+//             case 1:
+//                 sales_per_months[0] += sale.total_purchased
+//             break;
+//             case 2:
+//                 sales_per_months[1] += sale.total_purchased
+//             break;
+//             case 3:
+//                 sales_per_months[2] += sale.total_purchased
+//             break;
+//             case 4:
+//                 sales_per_months[3] += sale.total_purchased
+//             break;
+//             case 5:
+//                 sales_per_months[4] += sale.total_purchased
+//             break;
+//             case 6:
+//                 sales_per_months[5] += sale.total_purchased
+//             break;
+//             case 7:
+//                 sales_per_months[6] += sale.total_purchased
+//             break;
+//             case 8:
+//                 sales_per_months[7] += sale.total_purchased
+//             break;
+//             case 9:
+//                 sales_per_months[8] += sale.total_purchased
+//             break;
+//             case 10:
+//                 sales_per_months[9] += sale.total_purchased
+//             break;
+//             case 11:
+//                 sales_per_months[10] += sale.total_purchased
+//             break;
+//             case 12:
+//                 sales_per_months[11] += sale.total_purchased
+//             break;
+//         }
 
-    });
-    return sales_per_months;
-}
+//     });
+//     return sales_per_months;
+// }
 
 function isAuthenticated(req,res,next){
     if(req.isAuthenticated()){
@@ -287,9 +285,6 @@ router.post('/administrarCursos/editar',  upload.single('logoEditar'), async (re
     }
 
 });
-
-
-
 
 
 module.exports = router;
