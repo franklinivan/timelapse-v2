7abd125189259cad0d632dd439304ebdd345b9cd
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-01-15 15:49:18 -0500

    Mejorando la plantilla del admin, con sus secciones

--- src/router/rutasLoginRegistro.js
+++ src/router/rutasLoginRegistro.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const passport = require('passport');
-const mongoose = require('mongoose');
 
 router.get('/userType',(req,res) => {
     console.log(req.body);
